2da54de440f503efb8b9504baecf7f267639c992
aikokendo <alejandrameraz@gmail.com>
2016-10-16 00:33:07 +0800

    organized the code a bit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import tkinter
+import roles
+import canvas
+
+
+#initilize global variables
+global state,gameroles;
+gameroles = roles.roles([1,2],['blue','red'],[1,0])
+state = [[0 for i in range(0,16)] for i in range(0,16)]
+
+w = canvas.canvasManager(gameroles,state)
+
+
+
+
