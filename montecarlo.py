acff43472b099d8d18f127e6fa7d84b8131430c7
aikokendo <alejandrameraz@gmail.com>
2016-10-16 01:56:46 +0800

    defined basic structure for montecarlo. To be completed once discussed with the team.

--- montecarlo.py
+++ montecarlo.py
@@ -6,13 +6,18 @@ class MonteCarlo:
 
     def select(self):
         a=1
+        #select node for expansion based on the visit count.
 
     def expand(self):
         a=1
+        #expand a selectd node by all the possible actions it has available.
 
     def simulate(self):
         a=1
+        #simulate player movements up to getting a winner. Assign a value to the result (depthcharge)
+        #use simulate to best guess how much valuable a branch is.
 
     def back_propagation(self):
         a=1
+        #update parents based on children simulate results
 
