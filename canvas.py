f0048b2917c0b0f6ea46fa42c0bf4ae890644461
aikokendo <alejandrameraz@gmail.com>
2016-10-16 01:11:54 +0800

    made AI turn do random assignments.

--- canvas.py
+++ canvas.py
@@ -9,9 +9,10 @@ class canvasManager:
 
 
     def OnCanvasClick(self,event):
-        xboardpos = round((event.x - self.xoffset) / self.squaresize)
-        yboardpos = round((event.y - self.yoffset) / self.squaresize)
-        self.AddPiece(xboardpos,yboardpos)
+        if self.canvasblocked == 0:
+            xboardpos = round((event.x - self.xoffset) / self.squaresize)
+            yboardpos = round((event.y - self.yoffset) / self.squaresize)
+            self.AddPiece(xboardpos,yboardpos)
 
     def __init__(self,gameroles,state):
         self.xoffset = 96  # 300px image x center position - (460px image width / 2) + 26px minor image margin up to first line
@@ -19,18 +20,24 @@ class canvasManager:
         self.squaresize = 27  # square size
         self.piecesize = self.squaresize - 2
         self.numberoflines = 15
+        self.canvasblocked = 0
+        self.first = 0
         self.gameroles = gameroles
         self.state = state
 
-        top = tkinter.Tk()
-        top.title = "hola"
-        self.w = tkinter.Canvas(top, width=600, height=600)
+
+        self.top = tkinter.Tk()
+        self.top.title = "hola"
+        self.w = tkinter.Canvas(self.top, width=600, height=600)
         self.w.pack()
         photo = tkinter.PhotoImage(file='./board.png')
+        self.w.create_text(300,50,text="...")
         self.w.create_image(300, 300, image=photo)
         self.w.bind('<Button-1>', self.OnCanvasClick)
+        print(self.gameroles.GetCurrentColor())
+        self.CheckAI()
+        self.top.mainloop()
 
-        top.mainloop()
 
     def AddPiece(self,xboardpos,yboardpos):
         newx = self.xoffset + (xboardpos) * self.squaresize
@@ -40,13 +47,26 @@ class canvasManager:
                 obj = self.w.create_oval(newx - self.squaresize / 2, newy - +self.piecesize / 2, newx + self.piecesize / 2,
                                                newy + self.piecesize / 2, fill=self.gameroles.GetCurrentColor())
                 self.state[yboardpos][xboardpos] = 1
+                self.top.update()
                 self.NextTurn()
 
     def NextTurn(self):
         self.gameroles.NextPlayer()
+        self.canvasblocked = 0
+        self.CheckAI()
+
+
+    def CheckAI(self):
         if self.gameroles.GetCurrentAI() == 1:
+            self.canvasblocked = 1
             print('ai turn')
-            #activate monte carlo
-            #add piece as recommended by monte carlo.
-            #delete the random below
-            #self.add_piece(random.randint(0,15),random.randint(0,15))
+            if self.first == 0 :
+                self.first = 1
+                self.AddPiece(random.randint(7, 9), random.randint(7, 9))
+            else:
+                #activate monte carlo
+                #add piece as recommended by monte carlo.
+                #delete the random below
+                time.sleep(2)
+                self.AddPiece(random.randint(0,15),random.randint(0,15))
+
