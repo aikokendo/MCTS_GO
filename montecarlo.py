29f7916ee288c3eec03b7947382372648d450f5f
Luis Robles <roblescoulter@gmail.com>
2016-10-24 21:42:13 +0800

    added is_terminal method to check if there is a winner in the board

--- montecarlo.py
+++ montecarlo.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python
 import node
-import datetime
 
 
 class MonteCarlo:
 
-    def __init__(self,state):
-        # create the root
-        v0 = node.Node(state, None, None, 0, 0, None, [])
-        start_time = datetime.datetime.now()
-        while (datetime.datetime.now() - start_time).seconds < 8:
-            #v1 = self.select(v0)
-            #score = self.simulate(v1.state)
-            #v1.back_propagate(score)
-        #return x, y
+    def select(self, v):
+        while v #is non terminal
+            if v #is not fully expanded
+                return expand(v)
+            else
 
 
-    def select(self):
-        return 1
         #select node for expansion based on the visit count.
 
     def expand(self):
@@ -32,4 +25,3 @@ class MonteCarlo:
     def back_propagation(self):
         a=1
         #update parents based on children simulate results
-
